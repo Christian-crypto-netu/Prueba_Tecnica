fb63daf8e3eb044bccd32000bafae3917124db92
dteik1ng <posadaluis451@gmail.com>
2023-10-03 10:03:07 -0500

    mostrar las series en el componente de series agregado

--- src/app/components/series/series.component.ts
+++ src/app/components/series/series.component.ts
@@ -7,13 +7,43 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./series.component.css']
 })
 export class SeriesComponent implements OnInit {
-  jsonData: any;
+  jsonData: any[] = [];
+  selectedSeries: any | null = null;
+
 
   constructor(private http: HttpClient) {}
+  
 
   ngOnInit(): void {
-      this.http.get('./assets/sample.json').subscribe(data => {
-        this.jsonData = data;
-      })
+    this.http.get('./assets/sample.json').subscribe((data: any) => {
+      // Filtrar y ordenar los resultados
+      const filteredResults = data.entries
+        .filter((entry: any) => entry.releaseYear >= 2010 && entry.programType === 'series')
+        .sort((a: any, b: any) => a.title.localeCompare(b.title));
+      
+      // Tomar los primeros 20 resultados
+      this.jsonData = filteredResults.slice(0, 20);
+    });
   }
-}
+
+  onMouseOver(index: number) {
+    // Aplicar efecto de reducción de opacidad y borde blanco al posicionar el mouse sobre el resultado
+    document.getElementsByClassName('result-box')[index].classList.add('hovered');
+  }
+  
+  onMouseOut(index: number) {
+    // Remover efecto de reducción de opacidad y borde blanco al quitar el mouse del resultado
+    document.getElementsByClassName('result-box')[index].classList.remove('hovered');
+  }
+  
+  openPopup(series: any) {
+    // Abrir un popup mostrando la información completa de la serie seleccionada
+    this.selectedSeries = series;
+  }
+  closePopup() {
+    this.selectedSeries = null;
+  }
+    
+  
+
+  }
